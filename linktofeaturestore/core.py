18ea781cac0ae7c1652d1f22bd7e4bbdfbe62020
leepand <leepand@leepanddeMacBook-Pro.local>
2023-05-04 15:54:54 +0800

    update

--- linktofeaturestore/core.py
+++ linktofeaturestore/core.py
@@ -21,6 +21,9 @@ class LinkToFeatureStore:
             self.engine = sqla.create_engine(url)
 
         self.feature_db = make(f"db/{name}-v{version}", db_type="sqlite")
+        self.feature_cache = make(
+            f"cache/{name}-v{version}", db_name="feature_cache.db"
+        )
         self.build()
 
     def build(self):
@@ -35,15 +38,65 @@ class LinkToFeatureStore:
         finally:
             pass
 
-    def add(self, loop, kind="feature_set"):
-        self.feature_db.store(loop, kind)
+    def add(self, loop, kind="feature_set", uniadd=True):
+        uid = loop.uid
+        name = loop.name
+        version = loop.version
+        uni_key = f"{uid}:{name}:{version}"
+        if uniadd:
+            uni_saved = self.feature_cache.get(uni_key)
+            if uni_saved:
+                feature_type = loop.feature_type
+                cn_name = loop.cn_name
+                log_type = loop.log_type
+                author = loop.author
+                status = loop.status
+                content = loop.content
+                self.update_mutil(
+                    uid=uid,
+                    name=name,
+                    version=version,
+                    feature_type=feature_type,
+                    cn_name=cn_name,
+                    log_type=log_type,
+                    author=author,
+                    status=status,
+                    content=content,
+                )
+            else:
+                self.feature_db.store(loop, kind)
+                self.feature_cache.set(uni_key, "1")
+        else:
+            self.feature_db.store(loop, kind)
         return True
+
+    def get(self, uid, kind="feature_set"):
+        klass = {"feature_set": FeatureSet}[kind]
+        with self.session() as session:
+            return session.query(klass).filter_by(uid=str(uid)).first().to_dataclass()
+
+    def update_mutil(self, uid, name, version, **kwargs):
+        with self.session() as session:
+            # session.update(FeatureSet).where(FeatureSet.uid == uid).values(**kwargs)
+            created_at = dt.datetime.utcnow()
+            kwargs["created_at"] = created_at
+            stmt = (
+                update(FeatureSet)
+                .where(
+                    FeatureSet.uid == uid,
+                    FeatureSet.name == name,
+                    FeatureSet.version == version,
+                )
+                .values(**kwargs)
+            )
+            session.execute(stmt)
+            session.commit()
 
     def update(self, uid, **kwargs):
         with self.session() as session:
             # session.update(FeatureSet).where(FeatureSet.uid == uid).values(**kwargs)
             created_at = dt.datetime.utcnow()
-            kwargs["created_at"]=created_at
+            kwargs["created_at"] = created_at
             stmt = update(FeatureSet).where(FeatureSet.uid == uid).values(**kwargs)
             session.execute(stmt)
             session.commit()
---
+++ tests/test_fs.py
@@ -0,0 +1,20 @@
+from linktofeaturestore.core import LinkToFeatureStore
+from mlopskit import FeatureSet
+
+test = LinkToFeatureStore()
+
+feature = FeatureSet(
+    uid="21046110",
+    name="last_pay",
+    cn_name="上次付费",
+    author="孙迪",
+    status="running",
+    feature_type="float",
+    log_type="feature",
+    content="1.99",
+    version="1",
+)
+
+test.add(feature)
+
+test.update(uid="21046110", content="5.99")
